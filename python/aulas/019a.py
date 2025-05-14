f320045624f0a67359313af327b8ca381fe2912a
PedroWatermann <pedro.programmer.pt@gmail.com>
2025-05-13 21:27:04 -0300

    Teste commit PyCharm

---
+++ python/aulas/019a.py
@@ -0,0 +1,11 @@
+pessoas = dict()
+pessoas = {'nome': 'Gustavo', 'sexo': 'M', 'idade': 22}
+print(f'O {pessoas['nome']} tem {pessoas['idade']} anos.')
+print(pessoas.keys())
+print(pessoas.values())
+print(pessoas.items())
+for k, v in pessoas.items():
+    print(f'{k} = {v}')
+
+del pessoas['sexo']
+print(pessoas)
